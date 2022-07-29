dfa809172349cd0a592a9f9e2cb5eee10f885d2c
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 02:19:30 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -9,4 +9,6 @@ for i,v in pairs(HWIDTable1) do
     if v == HWIDVerif then
         print("Hardware ID Passed, you can load script now...")
     end
+    wait(2)
+    game.Players.LocalPlayer:Kick("Not a Valid Key.") wait(2) while true do print("no antikick") end
 end
