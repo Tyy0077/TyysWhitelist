91874a27da5db02b43ffbca76f5fe88649588277
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 03:58:08 -0400

    Update Encryption.lua

--- Encryption.lua
+++ Encryption.lua
@@ -9,3 +9,4 @@
 
     local verif2 = loadstring(game:HttpGet((verif), true))()
     local KeyC = getgenv().Key
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Tyy0077/TyysWhitelist/main/Encryption2.lua'), true))()
