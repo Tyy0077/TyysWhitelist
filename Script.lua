cdef797bcaa90d0d384d4613dce0337a383a0595
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 02:02:28 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,14 @@
-print("Ran script")
+print("Key Verif passed, moving on to Hardware verification.")
+local HWIDTable = syn.crypt.decrypt(
+    getgenv().Encrypted1,
+    getgenv().Key)
+
+local HWIDTable1 = loadstring(game:HttpGet(HWIDTable))()
+local HWIDVerif = game:GetService("RbxAnalyticsService"):GetClientId()
+setclipboard(HWID)
+for i,v in pairs(HWIDTable1) do
+    if v == HWIDVerif then
+        print("Hardware ID Passed, you can load script now...")
+    else game.Players.LocalPlayer:Kick("Not a Valid Key.") wait(2) while true do print("no antikick") end
+    end
+end
