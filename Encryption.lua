fab96179e1b5919cadbc4a3c689645dfe11f0eca
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 02:04:18 -0400

    Update Encryption.lua

--- Encryption.lua
+++ Encryption.lua
@@ -7,6 +7,10 @@ if getgenv().Key == '$nLliCMdi7gcynsFCK9u0aVNdtkNIiZA' then
         "https://zws.im/󠁳󠁿󠁣󠁹󠁿󠁢󠁰", 
         getgenv().Key)
     getgenv().Encrypted = enc
+    local enc1 = syn.crypt.encrypt(
+        "https://zws.im/‌󠁭󠁮󠁹󠁺󠁤󠁢",
+        getgenv().Key)
+    getgenv().Encrypted1 = enc1
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Tyy0077/TyysWhitelist/main/Decryption.lua"), true))()
     else game.Players.LocalPlayer:Kick("Not a Valid Key.") wait(2) while true do print("no antikick") end
 end
