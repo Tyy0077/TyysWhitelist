eb1c5353de5595dff008496efa0db83e12bbe994
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 00:17:24 -0400

    Update Encryption.lua

--- Encryption.lua
+++ Encryption.lua
@@ -1,11 +1,10 @@
-local Key1 = '$nLliCMdi7gcynsFCK9u0aVNdtkNIiZA'
-
+getgenv().Key = '$nLliCMdi7gcynsFCK9u0aVNdtkNIiZA'
 
 --<string> syn.crypt.encrypt(<string> data, <string> key) example
 
 
 local enc = syn.crypt.encrypt(
     "e", 
-    Key1)
-
+    getgenv().Key)
 setclipboard(enc)
+getgenv().Encrypted = enc
