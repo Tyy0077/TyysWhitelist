63998a1f2ec0e550723dbac6922188f903a6da0b
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 02:21:16 -0400

    Update Decryption.lua

--- Decryption.lua
+++ Decryption.lua
@@ -3,4 +3,8 @@ local mainscript = syn.crypt.decrypt(
     getgenv().Encrypted,
     getgenv().Key)
 
+local HWIDTable = syn.crypt.decrypt(
+    getgenv().Encrypted1,
+    getgenv().Key)
+
 loadstring(game:HttpGet((mainscript), true))()
