10e04fd1075212a111824ee9d573673eff6f8b96
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 04:01:35 -0400

    Update Encryption.lua

--- Encryption.lua
+++ Encryption.lua
@@ -1,12 +1,7 @@
-
-    local enc2 = syn.crypt.encrypt(
+  local enc2 = syn.crypt.encrypt(
         "https://zws.im/󠁧󠁤󠁫󠁫󠁤󠁤󠁹",
         "1")
     getgenv().Encrypted2 = enc2
     local verif = syn.crypt.decrypt(
         getgenv().Encrypted2,
         "1")
-
-    local verif2 = loadstring(game:HttpGet((verif), true))()
-    local KeyC = getgenv().Key
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Tyy0077/TyysWhitelist/main/Encryption2.lua'), true))()
