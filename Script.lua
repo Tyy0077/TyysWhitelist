849d44be25b3e343fd4462ae82fe13edba7be0b1
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-29 02:16:20 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -8,6 +8,5 @@ local HWIDVerif = game:GetService("RbxAnalyticsService"):GetClientId()
 for i,v in pairs(HWIDTable1) do
     if v == HWIDVerif then
         print("Hardware ID Passed, you can load script now...")
-    else game.Players.LocalPlayer:Kick("Not a Valid Key.") wait(2) while true do print("no antikick") end
     end
 end
