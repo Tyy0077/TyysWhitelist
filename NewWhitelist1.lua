28c1b9f15420dbd3533405add98bd3bf4c147bbf
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-30 21:31:21 -0400

    Update NewWhitelist1.lua

--- NewWhitelist1.lua
+++ NewWhitelist1.lua
@@ -1,4 +1,1 @@
-getgenv().keys =  {"JG5MbGlDTWRpN2djeW5zRkNLOXUwYVZOZHRrTklpWkE=", "Mg=="}
-getgenv().HWIDs = {"ODgyRkQzODgtMEFBRS00OTc3LTgxMjktOTM2RDBBRjc0QUU0", "Mg=="}
-getgenv().HWID = game:GetService("RbxAnalyticsService"):GetClientId()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Tyy0077/TyysWhitelist/main/NewWhitelist.lua'),true))()
+getgenv().keys =  {"JG5MbGlDTWRpN2djeW5zRkNLOXUwYVZOZHRrTklpWkE=", "Mg=="}getgenv().HWIDs = {"ODgyRkQzODgtMEFBRS00OTc3LTgxMjktOTM2RDBBRjc0QUU0", "Mg=="}getgenv().HWID = game:GetService("RbxAnalyticsService"):GetClientId()
