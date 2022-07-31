f48e017a6ee016f8e78a686d62b012fc36e16c49
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-30 21:26:14 -0400

    Update NewWhitelist.lua

--- NewWhitelist.lua
+++ NewWhitelist.lua
@@ -2,7 +2,20 @@ getgenv().KeyTrue = false if getgenv().KeyTrue then print('no script editing fag
 local Key = getgenv().Key
 for i,v in pairs(getgenv().keys) do 
     local a = syn.crypt.base64.decode(v)
-    if a == Key then getgenv().KeyTrue = true if a == Key and getgenv().KeyTrue then print('correct key')return end end
+    if a == Key then getgenv().KeyTrue = true if a == Key and getgenv().KeyTrue then print('correct key')end end
 end
-print('wrong key')
+if getgenv().KeyTrue == false then
+    print('wrong key')
+end
+if getgenv().KeyTrue then
+getgenv().HWIDTrue = false if getgenv().HWIDTrue then print('no script editing faggot')return end
+local HWID = getgenv().HWID
+for i,v in pairs(getgenv().HWIDs) do
+    local b = syn.crypt.base64.decode(v)
+    if b == HWID then getgenv().HWIDTrue = true if b == HWID and getgenv().HWIDTrue then print('correct hwid')end end
+end
+if getgenv().HWIDTrue == false then
+    print('wrong hwid')
+end
 return
+end
