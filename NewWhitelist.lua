68b5de767f7625b538ef5b0822d8a72170d1c38e
Tyy0077 <110214001+Tyy0077@users.noreply.github.com>
2022-07-30 20:37:25 -0400

    Update NewWhitelist.lua

--- NewWhitelist.lua
+++ NewWhitelist.lua
@@ -1,6 +1,6 @@
 getgenv().KeyTrue = false if getgenv().KeyTrue then print('no script editing faggot')return end
 local Key = getgenv().Key
-for i,v in pairs(Key) do 
+for i,v in pairs(keys) do 
     local a = syn.crypt.base64.decode(v)
     if a == Key then print('key found') return end
 end
